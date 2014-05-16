0620e03fadf09765b80bc48e504bdc76d6154f3b
RockyPan <pankkk@hotmail.com>
2014-05-16 14:49:41 +0800

    Initial Commit

---
+++ LearningPrj/LPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LPAppDelegate.h
+//  LearningPrj
+//
+//  Created by PanKyle on 14-5-16.
+//  Copyright (c) 2014年 PanKyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearningPrj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearningPrj
+//
+//  Created by PanKyle on 14-5-16.
+//  Copyright (c) 2014年 PanKyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LPAppDelegate class]));
+    }
+}
